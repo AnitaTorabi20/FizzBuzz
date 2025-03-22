8ddf41f32e033029312531a5e3656ca6ad430a51
Anita <anitatorabi.aut@gmail.com>
2025-03-22 15:39:52 +0330

    fix problems

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int start = 1;
+        int end = 100;
+
+        for (int i = start; i <= end; i++) {
+            System.out.println(fizzBuzz(i));
+        }
+    }
+
+    public static String fizzBuzz(int number) {
+        String result = "";
+
+        if (isDivisibleBy(number, 3)) result += "Fizz";
+        if (isDivisibleBy(number, 5)) result += "Buzz";
+
+        return result.isEmpty() ? String.valueOf(number) : result;
+    }
+
+    public static boolean isDivisibleBy(int number, int divisor) {
+        return number % divisor == 0;
     }
 }
